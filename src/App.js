ac8f0b8b0a7490a53ba37d2ed21651283ff6367a
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-11-19 10:00:40 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form, Table } from "./components/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-background">
+      <div className="mx-auto max-w-screen-lg">
+        <div className="h-screen flex justify-between items-center">
+          <Form />
+          <Table />
+        </div>
+      </div>
     </div>
   );
 }
